9dcdd33355741972f858718189001471178f0b75
Camarederic <camarederic@mail.ru>
2022-10-04 10:03:58 +0300

    Passing data between Activities

--- app/src/main/java/com/example/passingdatabetweenactivitiesparcelablesandbundles_7/MainActivity.kt
+++ app/src/main/java/com/example/passingdatabetweenactivitiesparcelablesandbundles_7/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.passingdatabetweenactivitiesparcelablesandbundles_7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonApply.setOnClickListener {
+            val name = editTextName.text.toString()
+            val age = editTextAge.text.toString().toInt()
+            val country = editTextCountry.text.toString()
+            val person = Person(name, age, country)
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("EXTRA_PERSON", person)
+            startActivity(intent)
+        }
     }
 }
